f083c3a911889e55b04148af467cddc52cd81ef8
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-19 19:46:30 -0500

    Input class created

--- Game/Game/Circle.cs
+++ Game/Game/Circle.cs
@@ -8,7 +8,13 @@ namespace Game
 {
     class Circle
     {
-        public int x { get; set; }
-        public int y { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Circle()
+        {
+            X = 0;
+            Y = 0;
+        }
     }
 }
